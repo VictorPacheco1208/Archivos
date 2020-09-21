2c694702a4916eca9d037aa0e928ad824e6e6e9a
Victor Pacheco <victor.pacheco5895@alumnos.udg.mx>
2020-09-20 19:08:37 -0500

    Actividad_02

---
+++ Actividad_02/Actividad_02.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void mostrar(int n, char arreglo[5]){
+    int i, j;
+    for(j=0; j<n; j++){
+        printf("Arreglo numero %i\n", j+1);
+        for(i=0; i<5; i++){
+            printf("Caracter numero [%i]: %c\n",i+1, arreglo[i]);   
+        }
+        printf("\n");
+    }
+}
+ 
+struct personaje{
+    char nombre[100],tipo[100], fuerza[100], salud[100];
+}Personajes[5];
+ 
+int main(){
+    float promedio=0;
+    int datos[5], suma=0, opc, per, i, n, b;
+    char respuesta , a[5], arreglo[5];
+    do{
+            printf("***************************BIENVENIDO***************************\n");
+            printf("1-Para capturar numeros y calcular la suma total y el promedio\n");
+            printf("2-Para ejecutar la funcion implementada\n");
+            printf("3-Para asignar atributos a personajes\n");
+            printf("Selecciona entre los 3 programas: ");
+            scanf("%i", &opc);
+            printf("\n");
+        
+        switch(opc){
+            case 1:
+                for(i=0; i<5; i++){
+                    printf("Escribe el elemento %i: ", i+1);
+                    scanf("%i", &datos[i]); 
+                    suma += datos[i];
+                }   
+                    promedio += suma/5.0;
+                printf("\n");
+                printf("Estos son los numeros que escribiste: [%i,%i,%i,%i,%i]\n", datos[0], datos[1], datos[2], datos[3], datos[4]);       
+                printf("Resultado de la suma total: %i\n", suma);
+                printf("Resultado del promedio: %.2f\n\n\n", promedio);
+                break;  
+                
+            case 2:
+                printf("¿Cuantas veces quieres que el arreglo se repita? ");
+                    scanf("%i", &b);
+                for(i=0; i<5; i++){
+                    char actual;
+                    printf("Coloca solo un caracter %i: \n", i+1);
+                    scanf(" %c", &actual);
+                    a[i] = actual;
+                }
+                mostrar(b, a);
+                break;
+                    
+            case 3:
+                do{
+                    printf("�Cuantos personajes deseas almacenar? ");
+                    scanf("%i",&per);
+                    fflush(stdin);
+                        if(per>5){
+                            printf("No puedes almacenar mas de 5 personajes\n");
+                            }
+                }while(per>5);
+                    
+                for(i=0; i<per; i++){
+                    printf("Escribe el nombre del personaje %i: \n", i+1);
+                    gets(Personajes[i].nombre);
+                    printf("Escribe el tipo del personaje %i: \n", i+1);
+                    gets(Personajes[i].tipo);
+                    printf("Escribe la fuerza del personaje %i: \n", i+1);
+                    gets(Personajes[i].fuerza);
+                    printf("Escribe la salud del personaje %i: \n", i+1);
+                    gets(Personajes[i].salud);
+                    }
+                for(i=0; i<per; i++){
+                    printf("El nombre del personaje numero %i es: %s\n", i+1, Personajes[i].nombre);
+                    printf("El tipo de personaje numero %i es: %s\n", i+1, Personajes[i].tipo);
+                    printf("La fuerza del personaje numero %i es: %s\n", i+1, Personajes[i].fuerza);
+                    printf("La salud del personaje numero %i es: %s\n", i+1, Personajes[i].salud);
+                    printf("\n");
+                    }   
+                break;
+            }
+            fflush(stdin);
+            printf("Presiona la letra v para volver a escoger alguna opcion: ");
+            scanf("%c",&respuesta);
+            printf("\n");
+        }while(respuesta == 'v' || respuesta == 'V');
+    
+    system("pause");
+    return 0;
+}
