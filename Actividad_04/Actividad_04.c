662cbbde568ef4cfe9e305f5eb88bbb2c8774b47
Victor Pacheco <victor.pacheco5895@alumnos.udg.mx>
2020-09-20 21:31:04 -0500

    Atributos a personajes

--- Actividad_04/Actividad_04.c
+++ Actividad_04/Actividad_04.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+ 
 void mostrar(int n, char arreglo[]){
     for(size_t i=0; i<n; i++){
         printf("%s\n", arreglo);
     }
 }
 
-
+struct personaje{
+    char nombre[100],tipo[100], fuerza[100], salud[100];
+}Personajes[5];
+ 
 int main(){
     float promedio=0;
-    int datos[5], suma=0, opc, i, n;
-    char respuesta, arreglo[30];
+    int datos[5], suma=0, opc, per, i, n, b;
+    char respuesta , a[5], arreglo[20];
+
     do{
-            
+            printf("***************************BIENVENIDO***************************\n");
             printf("1-Para capturar numeros y calcular la suma total y el promedio\n");
             printf("2-Para ejecutar la funcion implementada\n");
+            printf("3-Para asignar atributos a personajes\n");
             printf("Selecciona entre los 3 programas: ");
             scanf("%i", &opc);
             printf("\n");
@@ -35,16 +39,42 @@ int main(){
                 printf("Resultado del promedio: %.2f\n\n\n", promedio);
                 break;  
                 
-            case 2:  
-                printf("Escribe una cadena de hasta 30 caracteres: ");
+            case 2:
+                printf("Escribe una cadena de hasta 20 caracteres: ");
                 fflush(stdin);
                 fgets(arreglo, sizeof(arreglo), stdin);
                 printf("¿Cuantas veces quieres que se repita? ");
                 scanf("%i", &n);
                 mostrar(n, arreglo);
                 break;
-
+                    
             case 3:
+                do{
+                    printf("�Cuantos personajes deseas almacenar? ");
+                    scanf("%i",&per);
+                    fflush(stdin);
+                        if(per>5){
+                            printf("No puedes almacenar mas de 5 personajes\n");
+                            }
+                }while(per>5);
+                    
+                for(i=0; i<per; i++){
+                    printf("Escribe el nombre del personaje %i: \n", i+1);
+                    gets(Personajes[i].nombre);
+                    printf("Escribe el tipo del personaje %i: \n", i+1);
+                    gets(Personajes[i].tipo);
+                    printf("Escribe la fuerza del personaje %i: \n", i+1);
+                    gets(Personajes[i].fuerza);
+                    printf("Escribe la salud del personaje %i: \n", i+1);
+                    gets(Personajes[i].salud);
+                    }
+                for(i=0; i<per; i++){
+                    printf("El nombre del personaje numero %i es: %s\n", i+1, Personajes[i].nombre);
+                    printf("El tipo de personaje numero %i es: %s\n", i+1, Personajes[i].tipo);
+                    printf("La fuerza del personaje numero %i es: %s\n", i+1, Personajes[i].fuerza);
+                    printf("La salud del personaje numero %i es: %s\n", i+1, Personajes[i].salud);
+                    printf("\n");
+                    }   
                 break;
             }
             fflush(stdin);
