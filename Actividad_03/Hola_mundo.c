fdb0105d031b8fa7c7ad30f224383c8af11aa267
Victor Pacheco <victor.pacheco5895@alumnos.udg.mx>
2020-09-20 19:16:34 -0500

    Capture la edad

--- Actividad_03/Hola_mundo.c
+++ Actividad_03/Hola_mundo.c
@@ -2,10 +2,15 @@
 
 int main(){
     char nombre[100];
+    unsigned int edad;
 
     printf("Hola mundo\n");
     printf("Nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
+    printf("Hola %s", nombre);
+    printf("Edad: ");
+    scanf("%u", &edad);
+    printf("Vas a cumplir %u", edad + 1);
 
     return 0;
 }
